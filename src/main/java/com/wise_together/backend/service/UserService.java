3aa76d36eeaf715db088c62c53816cf5285f41e2
KSmiyazawa <ramos.karensaki@gmail.com>
2025-04-16 15:45:25 +0900

    feat: implement JWT token generation

--- src/main/java/com/wise_together/backend/service/UserService.java
+++ src/main/java/com/wise_together/backend/service/UserService.java
@@ -3,6 +3,9 @@ package com.wise_together.backend.service;
 import com.wise_together.backend.entity.User;
 import com.wise_together.backend.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -12,10 +15,25 @@ public class UserService {
     @Autowired
     private UserRepository repo;
 
+    @Autowired
+    AuthenticationManager authManager;
+
+    @Autowired
+    JWTService service;
+
     private BCryptPasswordEncoder encoder = new BCryptPasswordEncoder(12);
 
     public User register (User user) {
         user.setPassword(encoder.encode(user.getPassword()));
         return repo.save(user);
+    }
+
+    public String verify(User user) {
+        Authentication authentication = authManager.authenticate(new UsernamePasswordAuthenticationToken(user.getUsername(), user.getPassword()));
+
+        if (authentication.isAuthenticated())
+            return JWTService.generateToken(user.getUsername());
+
+        return "Fail";
     }
 }
